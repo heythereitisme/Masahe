ade77e3e1cdaebfefb03ea5bd151644ce6e20d94
Cobi Reeves <cobi.reeves@gmail.com>
2022-12-21 16:07:08 -0700

    Added mongoose

---
+++ back-end/mongoose.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  dbName: process.env.MONGODB_DBNAME,
+  user: process.env.MONGODB_USER,
+  pass: process.env.MONGODB_PASSWORD,
+});
+
+console.log(`Connected to MongoDB database ${process.env.MONGODB_DBNAME}`);
+
+export default mongoose;
