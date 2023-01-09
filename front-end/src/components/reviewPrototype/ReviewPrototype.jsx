539cd4bee3602082fe9ce00c5821fede13839b39
Cobi Reeves <cobi.reeves@gmail.com>
2023-01-08 18:13:37 -0700

    user list

--- front-end/src/components/reviewPrototype/ReviewPrototype.jsx
+++ front-end/src/components/reviewPrototype/ReviewPrototype.jsx
@@ -1,52 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import "./style.css"
+import React, { useState, useEffect } from "react";
+import "./style.css";
 
 const ReviewPrototype = () => {
-    const [firstname, setFirstname] = useState("")
-    const [lastname, setLastname] = useState("")
-    
- 
-    const addUser = async() => {
-        console.log(firstname, lastname);
-        const req = await fetch("/api/user", {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              firstName: firstname,
-              lastName: lastname
-            })
-          })
-        const newUser = req.json()
-        console.log(newUser)
-    }
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [userList, setUserList] = useState([]);
 
-    const formSubmit = (e) => {
-        e.preventDefault()
-        
-        console.log(addUser())
-      };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const req = await fetch("/api/user");
+    const users = await req.json();
+    setUserList(users);
+  };
+
+  const addUser = async () => {
+    console.log(firstname, lastname);
+    const req = await fetch("/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: firstname,
+        lastName: lastname,
+      }),
+    });
+    const newUser = await req.json();
+    console.log(newUser.firstName, "added");
+    getUsers()
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    addUser();
+  };
+
+  const deleteUser = async(u) => {
+
+  }
 
   return (
     <div>
-        <form onSubmit={formSubmit} className="user-creation-form">
-            <span>Create User</span>
-            <input 
-                type="text" 
-                placeholder='first name'
-                onInput={(e) => setFirstname(e.target.value)}
-            />
-            <input 
-                type="text" 
-                placeholder='first name'
-                onInput={(e) => setLastname(e.target.value)}
-            />
-            <button type='submit'>Create</button>
-        </form>
-        
+      <form onSubmit={formSubmit} className="user-creation-form">
+        <span>Create User</span>
+        <input
+          type="text"
+          placeholder="first name"
+          onInput={(e) => setFirstname(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="first name"
+          onInput={(e) => setLastname(e.target.value)}
+        />
+        <button type="submit">Create</button>
+      </form>
+      <div className="user-collection">
+        {userList.map((u) => {
+          return(
+            <div key={u._id} className="user-box">
+              <span>Name: {u.firstName} {u.lastName}</span>
+              <button onClick={deleteUser(u)}>Delete</button>
+            </div>
+              )
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ReviewPrototype
+export default ReviewPrototype;
