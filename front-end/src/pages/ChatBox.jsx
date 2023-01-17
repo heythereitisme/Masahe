e6facf8cb1448ab1568a2e8fac2ede3838abfa0c
Abneet K Pabyal <Abneet.P@gmail.com>
2023-01-16 20:20:02 -0700

    initialized tailwindcss

--- front-end/src/pages/ChatBox.jsx
+++ front-end/src/pages/ChatBox.jsx
@@ -3,14 +3,10 @@ import { Link } from 'react-router-dom'
 
 function ChatBox() {
   return (
-    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-    <div class="shrink-0">
-    </div>
-    <div>
-      <div class="text-xl font-medium text-black">ChitChat</div>
-      <p class="text-slate-500">You have a new message!</p>
-    </div>
-  </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
   )
 }
 
