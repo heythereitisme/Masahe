fda3136967847c316f070df354b255e3cc81ad8a
Abneet K Pabyal <Abneet.P@gmail.com>
2023-01-22 18:41:29 -0700

    Navigation css and TOOL: config

--- front-end/tailwind.config.cjs
+++ front-end/tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+
+      extend: {},
+    },
+    colors: {
+      primary: " hsl(237, 69%, 26%)",
+      secondary: "hsl(340, 93%, 44%)",
+      accent: "hsl(47, 64%, 52%)",
+      base: " hsl(26, 100%, 50%)",
+    },
   },
   plugins: [require("daisyui")],
 };
