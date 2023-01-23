9df83eb09acdaeb54ffad7aff20e745375b27429
Abneet K Pabyal <Abneet.P@gmail.com>
2023-01-23 09:40:24 -0700

    Error with Config *FIXED*

--- front-end/tailwind.config.cjs
+++ front-end/tailwind.config.cjs
@@ -2,21 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-
-      extend: {},
-    },
-    colors: {
-      primary: " hsl(237, 69%, 26%)",
-      secondary: "hsl(340, 93%, 44%)",
-      accent: "hsl(47, 64%, 52%)",
-      base: " hsl(26, 100%, 50%)",
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
 };
