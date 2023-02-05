33785dbdc2823161f2c40f4e001f47e815a11aae
Cobi Reeves <cobi.reeves@gmail.com>
2023-02-04 19:17:56 -0700

    little fix, added feel

--- front-end/src/components/massageTherapist/Profile.jsx
+++ front-end/src/components/massageTherapist/Profile.jsx
@@ -60,7 +60,7 @@ const Profile = ({ value }) => {
       body: JSON.stringify(u)
     })
     const updated = await req.json()
-    console.log(updated.message)
+    alert(updated.message)
   }
 
   if (value >= 2) {
@@ -385,7 +385,8 @@ const Profile = ({ value }) => {
                           rows={3}
                           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                           placeholder="you@example.com"
-                          defaultValue={""}
+                          defaultValue={about}
+                          onChange={(e) => setAbout(e.target.value)}
                         />
                       </div>
                       <p className="mt-2 text-sm text-gray-500">
