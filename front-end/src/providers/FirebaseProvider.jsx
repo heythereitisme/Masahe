1ed0f650d4105ab6dc9df7e282afdd50f5d1e551
dhaneeshKK <dhaneesh.krishnan@gmail.com>
2023-02-06 20:14:49 -0700

    created masahe google credentials

--- front-end/src/providers/FirebaseProvider.jsx
+++ front-end/src/providers/FirebaseProvider.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+//const firebaseConfig = {
+//	apiKey: "AIzaSyAuCLlU1mEBDoQiA0mNaOf-XdWebLBW6I0",
+//	authDomain: "safeplace-health.firebaseapp.com",
+//	projectId: "safeplace-health",
+//	storageBucket: "safeplace-health.appspot.com",
+//	messagingSenderId: "734921115811",
+//	appId: "1:734921115811:web:c82494dcec1b8a79535716",
+//	measurementId: "G-CWX8LQY5PC",
+//};
+
 const firebaseConfig = {
-	apiKey: "AIzaSyAuCLlU1mEBDoQiA0mNaOf-XdWebLBW6I0",
-	authDomain: "safeplace-health.firebaseapp.com",
-	projectId: "safeplace-health",
-	storageBucket: "safeplace-health.appspot.com",
-	messagingSenderId: "734921115811",
-	appId: "1:734921115811:web:c82494dcec1b8a79535716",
-	measurementId: "G-CWX8LQY5PC",
+	apiKey: "AIzaSyBKdVi8t2-MASQwftLb11L9Z388ADJsNDA",
+	authDomain: "masahe-c9.firebaseapp.com",
+	projectId: "masahe-c9",
+	storageBucket: "masahe-c9.appspot.com",
+	messagingSenderId: "750119957523",
+	appId: "1:750119957523:web:339f4f2c015cc7790b3be6",
+	measurementId: "G-MM6C4KQF66",
 };
 
 const app = initializeApp(firebaseConfig);
