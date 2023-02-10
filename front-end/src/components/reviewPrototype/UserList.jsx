7c1418d3e5ae898396cee163ea21cd0e94ca3ec1
Cobi Reeves <cobi.reeves@gmail.com>
2023-02-10 08:43:11 -0700

    return of my code

--- front-end/src/components/reviewPrototype/UserList.jsx
+++ front-end/src/components/reviewPrototype/UserList.jsx
@@ -105,26 +105,23 @@ const UserList = ({ mt }) => {
         return b[f] - a[f];
       });
       setFilteredUsers(sort);
-    } else if(f === "open"){
+    } else if (f === "open" || f === "licensed") {
       const sort = filteredData.sort((a, b) => {
-        return a[f] - b[f]
-      })
-      setFilteredUsers(sort)
+        return b[f] - a[f];
+      });
+      setFilteredUsers(sort);
+    } else {
+      const sort = filteredData.sort((a, b) => {
+        if (a.quadrant.includes(f)) {
+          return -1;
+        } else if (b.quadrant.includes(f)) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+      setFilteredUsers(sort);
     }
-    // else {
-    //   const sort = filteredData.sort((a, b) => {
-    //     let fa = a[f].toLowerCase(),
-    //       fb = b[f].toLowerCase();
-    //     if (fa < fb) {
-    //       return -1;
-    //     }
-    //     if (fa > fb) {
-    //       return 1;
-    //     }
-    //     return 0;
-    //   });
-    //   setFilteredUsers(sort);
-    // }
   };
 
   const detailPage = (u) => {
@@ -390,54 +387,71 @@ const UserList = ({ mt }) => {
     } else {
       return (
         <div className="bg-white min-h-screen">
-           <div className="flex items-center ">
+          <div className="flex items-center ">
             <div className="flex space-x-1 m-4">
-                <input
-                    type="text"
-                    className="block w-full px-4 py-2 text-accent bg-white border rounded-full focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-10"
-                    placeholder="Search..."
-                    onInput={(e) => setFilter(e.target.value)}
-                />
-                <button className="px-4 text-white bg-neutral rounded-full ">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                        />
-                    </svg>
-                </button>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 text-accent bg-white border rounded-full focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-10"
+                placeholder="Search..."
+                onInput={(e) => setFilter(e.target.value)}
+              />
+              <button className="px-4 text-white bg-neutral rounded-full ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
             </div>
-        </div>
-      
+          </div>
+
           <div className="mb-10 ml-4">
+            <span className="text-2xl">Sort By:</span>
             <button
               className="btn btn-secondary m-1 font-title"
               onClick={() => sorter("avgRating")}
             >
               Rating
             </button>
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-secondary m-1 font-title">
+                Quadrant
+              </label>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu p-2 shadow bg-white rounded-box w-48 text-primary"
+              >
+                <li>
+                  <button onClick={() => sorter("NE")}>Northeast</button>{" "}
+                </li>
+                <li>
+                  <button onClick={() => sorter("NW")}>Northwest</button>{" "}
+                </li>
+                <li>
+                  <button onClick={() => sorter("SE")}>Southeast</button>{" "}
+                </li>
+                <li>
+                  <button onClick={() => sorter("SW")}>Southwest</button>{" "}
+                </li>
+              </ul>
+            </div>
             <button
-              className="bg-red-400 hover:bg-blue-400 p-1 m-1 rounded-2xl w-24"
-              onClick={() => sorter("firstName")}
+              className="btn btn-secondary m-1 font-title"
+              onClick={() => sorter("licensed")}
             >
-              Quadrant
+              Licensed
             </button>
             <button
-              className="bg-red-400 hover:bg-blue-400 p-1 m-1 rounded-2xl w-24"
-              onClick={() => sorter("certification")}
-            >
-              Certification
-            </button>
-            <button
-              className="bg-red-400 hover:bg-blue-400 p-1 m-1 rounded-2xl w-24"
+              className="btn btn-secondary m-1 font-title"
               onClick={() => sorter("open")}
             >
               Available
@@ -445,25 +459,67 @@ const UserList = ({ mt }) => {
           </div>
           <div className="grid grid-cols-4 gap-2 mt-5">
             {pages[currentPage].map((u) => {
+              const rating = Math.round(u.avgRating);
               return (
                 <div
                   key={u.username}
-                  className=" bg-slate-50 shadow-2xl flex flex-col rounded-lg p-4 ml-4"
+                  className=" bg-slate-100 shadow-2xl flex flex-col rounded-lg p-4 ml-4"
                 >
                   <span className="font-heading text-lg mb-3 text-primary">
-                  {u.firstName} {u.lastName}
+                    {u.firstName} {u.lastName} 
                   </span>
                   <span className="font-title text-primary">{u.username}</span>
-                  <span className=" font-title text-primary">Rating: {u.avgRating} 
-                  <div className="rating">
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-</div>
-</span>
-                  {u.open && <span className="font-title text-primary font-bold mt-3 mb-1">Appointments available!</span>}
+                  <span className="font-title text-primary">Quadrant(s):{u.quadrant.map((q) => <span className="ml-1 mr-1">{q}</span>)}</span>
+                  <span className=" font-title text-primary">
+                    <div className="rating">
+                      <input
+                        type="radio"
+                        value={1}
+                        className="mask mask-star-2 bg-orange-400"
+                        checked={rating === 1}
+                        readOnly
+                      />
+                      <input
+                        type="radio"
+                        className="mask mask-star-2 bg-orange-400"
+                        value={2}
+                        checked={rating === 2}
+                        readOnly
+                      />
+                      <input
+                        type="radio"
+                        name="rating-2"
+                        value={3}
+                        className="mask mask-star-2 bg-orange-400"
+                        checked={rating === 3}
+                        readOnly
+                      />
+                      <input
+                        type="radio"
+                        value={4}
+                        className="mask mask-star-2 bg-orange-400"
+                        checked={rating === 4}
+                        readOnly
+                      />
+                      <input
+                        type="radio"
+                        value={5}
+                        className="mask mask-star-2 bg-orange-400"
+                        checked={rating === 5}
+                        readOnly
+                      />
+                    </div>
+                  </span>
+                  {u.licensed && (
+                    <span className="font-title text-primary mt-3 mb-1">
+                      Licensed
+                    </span>
+                  )}
+                  {u.open && (
+                    <span className="font-title text-primary font-bold mt-3 mb-1">
+                      Appointments available!
+                    </span>
+                  )}
                   <div className="button-holder flex justify-around ">
                     <button
                       onClick={() => mtDetailPage(u.username)}
@@ -481,30 +537,54 @@ const UserList = ({ mt }) => {
                       Message
                     </button>
                   </div>
-                  
+
                   <form
+                    onChange={(e) => setRating(e.target.value)}
                     onSubmit={(e) => submitRating(e, u)}
                     className="flex justify-between m-5 items-center"
                   >
-                    
                     <div className="rating">
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-</div>
-     <div>
-                    <button
-                      type="submit"
-                      className="btn btn-secondary font-title"
-                    >
-                      Submit
-                    </button>
-      </div>
+                      <input
+                        type="radio"
+                        name="rating-2"
+                        value={1}
+                        className="mask mask-star-2 bg-orange-400"
+                      />
+                      <input
+                        type="radio"
+                        name="rating-2"
+                        value={2}
+                        className="mask mask-star-2 bg-orange-400"
+                      />
+                      <input
+                        type="radio"
+                        name="rating-2"
+                        value={3}
+                        className="mask mask-star-2 bg-orange-400"
+                        defaultChecked
+                      />
+                      <input
+                        type="radio"
+                        name="rating-2"
+                        value={4}
+                        className="mask mask-star-2 bg-orange-400"
+                      />
+                      <input
+                        type="radio"
+                        name="rating-2"
+                        value={5}
+                        className="mask mask-star-2 bg-orange-400"
+                      />
+                    </div>
+                    <div>
+                      <button
+                        type="submit"
+                        className="btn btn-secondary font-title"
+                      >
+                        Submit Rating
+                      </button>
+                    </div>
                   </form>
-                  
-
                 </div>
               );
             })}
