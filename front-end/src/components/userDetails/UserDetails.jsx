184e18072c45e366576f42e2ec0f50f64b9ed4df
Cobi Reeves <cobi.reeves@gmail.com>
2023-02-15 14:41:02 -0700

    consolidated details a bit

--- front-end/src/components/userDetails/UserDetails.jsx
+++ front-end/src/components/userDetails/UserDetails.jsx
@@ -93,44 +93,21 @@ const UserDetails = () => {
               if(user.ratedUser.permission === permission){
                 return <AuthFailure />
               }
-              if(user.ratedUser.permission === 1){
                 return (
                   <div className='bg-white min-h-screen text-center flex flex-col'>
-                    {console.log(user.ratedUser)}
                           <h3 className='text-5xl m-1 font-heading text-primary'> {user.ratedUser.firstName} {user.ratedUser.lastName}</h3>
                       <span className='text-xl m-1 text-primary'>
-                          {user.ratedUser.avgRating}   
+                          Rating: {Math.round((user.ratedUser.avgRating * 100) /100)}   
                       </span>
                       <form onSubmit={formSubmit} className='flex flex-col w-screen mb-2 '>
                           <span className='text-5xl m-5 font-heading text-secondary'>Notes</span>
                           <textarea value={notes} onChange={(e) => setNotes(e.target.value)} className='w-2/3 min-h-[50vh] mx-auto bg-slate-100 rounded-lg drop-shadow-xl text-primary p-5'/>
-
-                          <button type='submit' className='btn btn-primary font-title p-4 bg-white text-primary hover:text-white mt-5 w-48 mx-auto'>Save</button>
-              
-                      </form>
-                     <div> <button onClick={returnPage} className='btn btn-secondary font-title p-3 bg-white text-primary hover:text-white mt-2 w-48 mx-auto'>Back</button> </div>
-                  </div>
-              )
-              } else if (user.ratedUser.permission === 2) {
-                return(
-                  <div className='bg-white min-h-screen text-center flex flex-col'>
-                          <h3 className='text-5xl m-1 font-heading text-primary'>Name: {user.ratedUser.firstName} {user.ratedUser.lastName}</h3>
-                          <span className='text-xl m-1 text-primary'>
-                          Rating: {user.ratedUser.avgRating}   
-                      </span>
-                      <form onSubmit={formSubmit} className='flex flex-col w-screen mb-2'>
-                          <span>Notes:</span>
-                          <textarea value={notes} onChange={(e) => setNotes(e.target.value)} className='w-2/3 min-h-[50vh] mx-auto bg-slate-100 rounded-lg drop-shadow-xl text-primary p-5'/>
                           <button type='submit' className='btn btn-primary font-title p-4 bg-white text-primary hover:text-white mt-5 w-48 mx-auto'>Save</button>
                       </form>
-                     
-                      <button onClick={() => bookUser(user.ratedUser.username)} className='btn btn-secondary font-title p-3 bg-white text-primary hover:text-white mt-2 w-48 mx-auto'>Book</button>
-                      <button onClick={returnPage} className='btn btn-secondary font-title p-3 bg-white text-primary hover:text-white mt-2 w-48 mx-auto'>Back</button>
-                   
+                      {user.ratedUser.open && <button onClick={() => bookUser(user.ratedUser.username)} className='btn btn-secondary font-title p-3 bg-white text-primary hover:text-white mt-2 w-48 mx-auto'>Book</button>}
+                     <button onClick={returnPage} className='btn btn-secondary font-title p-3 bg-white text-primary hover:text-white mt-2 w-48 mx-auto'>Back</button>
                   </div>
-              )
-              }
-              
+              )  
             }
 }
 
