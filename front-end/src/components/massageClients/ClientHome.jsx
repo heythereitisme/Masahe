dc94be870444ce37bf965ede6ec52b4802979b44
Abneet K Pabyal <Abneet.P@gmail.com>
2023-02-16 08:56:46 -0700

    Ui: images

--- front-end/src/components/massageClients/ClientHome.jsx
+++ front-end/src/components/massageClients/ClientHome.jsx
@@ -65,8 +65,8 @@ const rateUser = async (u) => {
         <span className='text-3xl font-heading text-primary'>Services</span>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-10">
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Massage" /></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Head-Massage.jpg" alt="Massage" className='w-full h-60 object-cover' /></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Swedish Massage</h2>
     <p className=' font-body text-primary mb-2'>A gentle full-body massage.</p>
@@ -76,8 +76,8 @@ const rateUser = async (u) => {
     </div>
 </div>
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Massage" /></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Hotstone.jpg" alt="Massage" className='w-full h-60 object-cover' /></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Hot Stone Massage</h2>
     <p className=' font-body text-primary mb-2'>This type of massage involves the use and placement of basalt River rock on different points of the body.</p>
@@ -87,8 +87,8 @@ const rateUser = async (u) => {
     </div>
 </div>
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Massage" /></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Back-Massage.jpg" alt="Massage" className='w-full h-60 object-cover' /></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Deep tissue massage</h2>
     <p className=' font-body text-primary mb-2'>Applying sustained pressure using slow, deep strokes to target the inner layers of your muscles and connective tissues.</p>
@@ -98,8 +98,8 @@ const rateUser = async (u) => {
     </div>
 </div>
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Massage" /></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Candle.jpg" alt="Massage" className='w-full h-60 object-cover' /></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Aromatherapy</h2>
     <p className=' font-body text-primary mb-2'>Using essential oils for therapeutic benefit.</p>
@@ -109,8 +109,8 @@ const rateUser = async (u) => {
     </div>
 </div>
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Massage" /></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Foot-Massage.jpg" alt="Massage" className='w-full h-60 object-cover' /></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Sports massage</h2>
     <p className=' font-body text-primary mb-2'>Those with repetitive use injury to a muscle, generally use to ease sports injuries.</p>
@@ -120,8 +120,8 @@ const rateUser = async (u) => {
     </div>
 </div>
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Massage" /></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Neck-Massage.jpg" alt="Massage" className='w-full h-60 object-cover' /></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Reflexology</h2>
     <p className=' font-body text-primary mb-2'>Reflexology uses gentle to firm pressure on different pressure points of the feet, hands, and ears.</p>
@@ -131,8 +131,8 @@ const rateUser = async (u) => {
     </div>
 </div>
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Massage" /></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Back-Massage.jpg" alt="Massage" className='w-full h-60 object-cover' /></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Lymphatic drainage massage</h2>
     <p className=' font-body text-primary mb-2'>Gentle massage that encourages the lymph fluids to circulate through the body.</p>
@@ -142,8 +142,8 @@ const rateUser = async (u) => {
     </div>
 </div>
 
-<div className="card w-96 bg-slate-50 shadow-xl ml-4">
-    <figure><img src="/services/Acupuncture.jpg" alt="Massage" className='bg-cover bg-scroll'/></figure>
+<div className="card w-96 bg-slate-50 shadow-xl ml-1 md:ml-4">
+    <figure><img src="/services/Acupuncture.jpg" alt="Massage" className='w-full h-60 object-cover'/></figure>
     <div className="card-body">
     <h2 className="card-title flex justify-center text-primary">Acupuncture</h2>
     <p className=' font-body text-primary mb-2'> Insertion of very thin needles through your skin at strategic points on your body.</p>
